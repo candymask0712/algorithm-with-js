025950a46b1933ab5e7af4351da269ff66de5f6c
candymask0712 <candymask0712@gmail.com>
2024-09-18 14:29:40 +0900

    refactor: 올바른 괄호 풀이 수정

--- stack_queue/pro_lv1_correct-brackets.js
+++ stack_queue/pro_lv1_correct-brackets.js
@@ -1,11 +1,10 @@
 // * 최근 나의 풀이 (24.09.18)
 function solution(s) {
-  var answer = true;
   let count = 0;
   const stack = s.split('');
+
   for (const bracket of stack) {
-    if (bracket === '(') count++;
-    else count--;
+    bracket === '(' ? count++ : count--;
     if (count < 0) return false;
   }
 
