7aef833be17035fa82842680d70c643c6b19907d
candymask0712 <candymask0712@gmail.com>
2024-09-29 19:00:32 +0900

    solve: LCS 문제 풀이

--- DP/book_15-2_LCS.js
+++ DP/book_15-2_LCS.js
@@ -2,8 +2,10 @@ function solution(str1, str2){
   let answer
   const n = str1.length
   const m = str2.length
+  // ! DP의 특성상 i - 1 처럼 이전요소를 참조하는 경우가 있어서 1부터 시작
   let dp = Array.from(Array(n+1), ()=>Array(m+1).fill(0))
-
+  
+  // ! DP의 특성상 i - 1 처럼 이전요소를 참조하는 경우가 있어서 1부터 시작
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= m; j++) {
       const char1 = str1[i-1]
@@ -16,12 +18,9 @@ function solution(str1, str2){
       }
     }
   }
-
-  console.table(dp)
-
+  // ! 불필요한 식이고 dp[n][m]이 최대 길이임
   return Math.max(...dp.map(row => Math.max(...row)))
 }
-
 
 console.log(solution("ABCBDAB","BDCAB")) // 4
 console.log(solution("AGGTAB","GXTXAYB")) // 4
