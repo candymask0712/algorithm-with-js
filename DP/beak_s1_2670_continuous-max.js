124dcb05b100a5991d0cbfa848610ca327496fb5
candymask0712 <candymask0712@gmail.com>
2024-10-08 10:22:14 +0900

    solve: 백준 dp 문제들

---
+++ DP/beak_s1_2670_continuous-max.js
@@ -0,0 +1,30 @@
+// 풀이 템플릿
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   const N = parseInt(input[0]); // 실수들의 개수
+//   const numbers = [];
+
+//   for (let i = 1; i <= N; i++) {
+//     numbers.push(parseFloat(input[i]));
+//   }
+
+//   const result = solution(N, numbers);
+//   console.log(result.toFixed(3)); // 소수점 셋째 자리까지 출력
+
+//   process.exit();
+// });
+
+// function solution(N, numbers) {
+//   return result;
+// }
---
+++ DP/beak_s3_9461_padoban-sequence.js
@@ -0,0 +1,60 @@
+// 파도반 수열 - 풀이 템플릿
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   const T = parseInt(input[0]); // 테스트 케이스의 개수
+
+//   for (let i = 1; i <= T; i++) {
+//     const N = parseInt(input[i]);
+//     const result = solution(N);
+//     console.log(result);
+//   }
+
+//   process.exit();
+// });
+
+// function solution(N) {}
+
+// 나의 풀이
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  const T = parseInt(input[0]); // 테스트 케이스의 개수
+
+  for (let i = 1; i <= T; i++) {
+    const N = parseInt(input[i]);
+    const result = solution(N);
+    console.log(result);
+  }
+
+  process.exit();
+});
+
+function solution(N) {
+  const dp = Array(N + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 1;
+  dp[3] = 1;
+  for (let i = 4; i <= N; i++) {
+    dp[i] = dp[i - 2] + dp[i - 3];
+  }
+  return dp[N];
+}
