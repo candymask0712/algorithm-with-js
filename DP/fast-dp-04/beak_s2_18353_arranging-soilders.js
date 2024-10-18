3f0c175d5e0ce9b916e67e6b603ace7236d2621a
candymask0712 <candymask0712@gmail.com>
2024-10-19 00:01:16 +0900

    test

---
+++ DP/fast-dp-04/beak_s2_18353_arranging-soilders.js
@@ -0,0 +1,23 @@
+// 풀이템플릿
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//   input.push(line);
+// }).on('close', function () {
+//   const n = parseInt(input[0]);
+//   const soldiers = input[1].split(' ').map(Number);
+  
+//   console.log(solution(n, soldiers));
+//   process.exit();
+// });
+
+// function solution(n, soldiers) {
+  
+//   return 0; 
+// }
--- 연습장.js
+++ 연습장.js
@@ -1,1 +1,10 @@
+function solution(n, soldiers) {
+  let answer = 0
+  for(let i=0; i<n; i++){
+   if(soldiers[i]<= soldiers[i+1]) answer++
+  }
+  return answer
+}
 
+console.info(solution(7, [15, 11, 4, 8, 5, 2, 4]))
+console.info(solution(7, [4, 2, 5]))
