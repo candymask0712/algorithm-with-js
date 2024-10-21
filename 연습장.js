591ce674cf92c95ab976b1bfeff9f295dd28a075
candymask0712 <candymask0712@gmail.com>
2024-10-21 22:34:00 +0900

    solve: 1로 만들기 문제 풀이

--- 연습장.js
+++ 연습장.js
@@ -1,10 +1,38 @@
-function solution(n, soldiers) {
-  let answer = 0
-  for(let i=0; i<n; i++){
-   if(soldiers[i]<= soldiers[i+1]) answer++
+function solution(N) {
+  const memo = new Map();
+
+  function BFS() {
+    let queue = [[N, 0]];
+    memo.set(N, 0);
+
+    while (queue.length) {
+      const [num, trial] = queue.shift();
+
+      if (num === 1) {
+        return trial;
+      }
+
+      const nextTrials = [
+        [num - 1, trial + 1],
+        [num % 2 === 0 ? num / 2 : null, trial + 1],
+        [num % 3 === 0 ? num / 3 : null, trial + 1]
+      ];
+
+      for (const [nextNum, nextTrial] of nextTrials) {
+        if (nextNum !== null && nextNum >= 1) {
+          if (!memo.has(nextNum) || memo.get(nextNum) > nextTrial) {
+            memo.set(nextNum, nextTrial);
+            queue.push([nextNum, nextTrial]);
+          }
+        }
+      }
+    }
+
+    return -1; // 1에 도달할 수 없는 경우
   }
-  return answer
+
+  return BFS();
 }
 
-console.info(solution(7, [15, 11, 4, 8, 5, 2, 4]))
-console.info(solution(7, [4, 2, 5]))
+console.info(solution(2)); // 1
+console.info(solution(10)); // 3
