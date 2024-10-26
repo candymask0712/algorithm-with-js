65bfa5f22d938f6e4477e45b2a7bbc88f301303a
candymask0712 <candymask0712@gmail.com>
2024-10-26 23:28:37 +0900

    문제풀이

--- 연습장.js
+++ 연습장.js
@@ -1,38 +1,24 @@
-function solution(N) {
-  const memo = new Map();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  function BFS() {
-    let queue = [[N, 0]];
-    memo.set(N, 0);
+let input = [];
 
-    while (queue.length) {
-      const [num, trial] = queue.shift();
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  console.log(solution(input));
+  process.exit();
+});
 
-      if (num === 1) {
-        return trial;
-      }
+function solution(input) {
+  const N = parseInt(input[0]);
+  const costs = input.slice(1).map((line) => line.split(' ').map(Number));
 
-      const nextTrials = [
-        [num - 1, trial + 1],
-        [num % 2 === 0 ? num / 2 : null, trial + 1],
-        [num % 3 === 0 ? num / 3 : null, trial + 1]
-      ];
+  // 여기에 문제 해결 로직을 구현하세요
 
-      for (const [nextNum, nextTrial] of nextTrials) {
-        if (nextNum !== null && nextNum >= 1) {
-          if (!memo.has(nextNum) || memo.get(nextNum) > nextTrial) {
-            memo.set(nextNum, nextTrial);
-            queue.push([nextNum, nextTrial]);
-          }
-        }
-      }
-    }
-
-    return -1; // 1에 도달할 수 없는 경우
-  }
-
-  return BFS();
+  // 예시 반환값 (실제 로직에 맞게 수정 필요)
+  return 0;
 }
-
-console.info(solution(2)); // 1
-console.info(solution(10)); // 3
