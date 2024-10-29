a15e571bd17cbf60ce6dbd61cb3cf5beafdc9cc7
candymask0712 <candymask0712@gmail.com>
2024-10-29 16:40:55 +0900

    chore: 연습장 문제풀이

--- 연습장.js
+++ 연습장.js
@@ -1,10 +1,11 @@
-function solution(n, soldiers) {
-  let answer = 0
-  for(let i=0; i<n; i++){
-   if(soldiers[i]<= soldiers[i+1]) answer++
-  }
-  return answer
+function solution(n, distances, prices) {
+  return prices.reduce((acc, price, index) => {
+    console.info(acc, price, index)
+      
+      if(index === prices.length -1) return acc
+      else return acc + distances[index]*price
+  }, 0)
 }
 
-console.info(solution(7, [15, 11, 4, 8, 5, 2, 4]))
-console.info(solution(7, [4, 2, 5]))
+console.info(solution(4, [2,3,1], [5,2,4,1])) // 18
+console.info(solution(4, [3,3,4], [1,1,1,1])) // 10
