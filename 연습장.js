5a05323c614128a3c34236bc6f27e808023839ea
candymask0712 <candymask0712@gmail.com>
2024-12-05 23:57:19 +0900

    chore: 문제 풀이용 템플릿 생성

--- 연습장.js
+++ 연습장.js
@@ -1,18 +1,32 @@
-function solution(n, heights) {
-  let answer=0
-  const arrows = []
-  heights.forEach(ballon =>{
-    // console.info(ballon, arrows)
-    if(arrows.includes(ballon)){
-      arrows[arrows.indexOf(ballon)] -= 1
-    }else{
-      arrows.push(ballon-1)
-    }
-  })
-  return arrows.length
-}
+function solution(x, y, n) {
+  var answer = 0;
 
-// 예시 테스트케이스 
-console.info(solution(5, [2, 1, 5, 4, 3])); // 2
-console.info(solution(5, [1, 2, 3, 4, 5])); // 5
-console.info(solution(5, [4, 5, 2, 1, 4])); // 3
+  function BFS() {
+    let queue = [];
+    queue.push([x, 0]);
+    while (queue.length) {
+      const current = queue.shift();
+      let [num, count] = current;
+      if (num === y) return count;
+      for (const next of [
+        ['+', n],
+        ['*', 2],
+        ['*', 3],
+      ]) {
+        const [operator, anotherNum] = next;
+        if (operator === '+') {
+          num += anotherNum;
+        } else {
+          num *= anotherNum;
+        }
+
+        if (num <= 1000000) {
+          queue.push([num, count + 1]);
+        }
+      }
+    }
+  }
+  BFS();
+
+  return answer;
+}
