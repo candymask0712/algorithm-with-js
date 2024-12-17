534b4c7ff8e39181ef3aec2d971a5dde0deab91f
candymask0712 <candymask0712@gmail.com>
2024-12-17 09:46:56 +0900

    chore: 풀이 수정

--- Implementation/pro_lv2_dividing-number-card.js
+++ Implementation/pro_lv2_dividing-number-card.js
@@ -33,21 +33,6 @@ function isConditionB(arrayA, arrayB, number) {
 }
 
 // 참고 풀이
-1;
-2;
-3;
-4;
-5;
-6;
-7;
-8;
-9;
-10;
-11;
-12;
-13;
-14;
-15;
 function solution(arrayA, arrayB) {
   const aResult = getAnswer(arrayA, arrayB);
   const bResult = getAnswer(arrayB, arrayA);
