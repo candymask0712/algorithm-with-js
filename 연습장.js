4a9932451e3ee3df4a9c5b57a2e1decbe624d7b7
candymask0712 <candymask0712@gmail.com>
2024-12-27 10:29:38 +0900

    chore: 연습장 풀이

--- 연습장.js
+++ 연습장.js
@@ -1,44 +1,18 @@
-function solution(order) {
+function solution(n) {
   let answer = 0;
-  const stack = [];
-  const n = order.length;
+
   for (let i = 1; i <= n; i++) {
-    const cur = order[0];
-    const top = stack[stack.length - 1];
-    // console.log(i, cur, top, answer, stack);
-    if (i === cur) {
-      order.shift();
-      answer++;
-    } else {
-      if (cur === top) {
-        stack.pop();
-        order.shift();
+    let num = i;
+    let sum = num;
+    while (sum <= n) {
+      if (sum === n) {
         answer++;
-        i--;
+        break;
       } else {
-        stack.push(i);
+        sum += ++num;
       }
-    }
-  }
-  // console.log(order);
-  // console.log(stack);
-  let i = 0;
-  while (stack.length && i < order.length) {
-    const cur = order[i];
-    const top = stack[stack.length - 1];
-    // console.log(cur, top);
-    if (cur === top) {
-      stack.pop();
-      answer++;
-      i++;
-    } else {
-      break;
     }
   }
 
   return answer;
 }
-
-console.info(solution([4, 3, 1, 2, 5])); // 2
-console.info(solution([5, 4, 3, 2, 1])); // 5
-console.info(solution([1, 2, 4, 3, 5])); // 5
