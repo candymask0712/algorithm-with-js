ae65c2bff52b96a707d93656aec814c8cdb6bcde
candymask0712 <candymask0712@gmail.com>
2024-12-29 14:22:24 +0900

    study: 예상대진표 학습

--- 복습필요/12월 4주차/pro_lv2_예상대진표.js
+++ 복습필요/12월 4주차/pro_lv2_예상대진표.js
@@ -1,5 +1,9 @@
+// * 첫 번째 풀이
 function solution(n, a, b) {
   let round = 1;
+  // while 조건은 두 명의 번호가 같아질 때 까지 반복
+  // 숫자가 같아졌다는 것은 이전 라운드에서 만났다는 의미
+  // ! 아래 조건은 a!==b로 처리 가능
   while (Math.ceil(a / 2) !== Math.ceil(b / 2)) {
     a = Math.ceil(a / 2);
     b = Math.ceil(b / 2);
