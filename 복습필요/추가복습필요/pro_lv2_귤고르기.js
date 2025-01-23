efee758d1bcfee1c3663204e024c321373766b0d
candymask0712 <candymask0712@gmail.com>
2025-01-23 17:38:38 +0900

    solve: 풀이 및 폴더 이동

---
+++ 복습필요/추가복습필요/pro_lv2_귤고르기.js
@@ -0,0 +1,62 @@
+// * 첫번째 풀이
+function solution(k, tangerine) {
+  let list = [];
+  tangerine.sort((a, b) => a - b);
+  let count = 1;
+  for (let i = 0; i < tangerine.length; i++) {
+    if (tangerine[i] !== tangerine[i + 1] || i === tangerine.length - 1) {
+      list.push(count);
+      count = 1;
+    } else count++;
+  }
+
+  list.sort((a, b) => b - a);
+
+  let sum = 0;
+  // ! 기존에는  i < list.lengt 조건
+  // ! 모두 종류가 다른 경우에 대한 처리가 불가능
+  for (let i = 0; i <= list.length; i++) {
+    if (sum >= k) return i;
+    else {
+      sum += list[i];
+    }
+  }
+}
+
+// * 두번째 풀이
+function solution(k, tangerine) {
+  var answer = 0;
+
+  let count = new Map();
+  const n = tangerine.length;
+
+  let sum = 1;
+
+  tangerine.sort((a, b) => a - b);
+
+  for (let i = 0; i < n; i++) {
+    const current = tangerine[i];
+    const next = tangerine[i + 1];
+    if (current !== next) {
+      count.set(current, sum);
+      sum = 1;
+    } else {
+      sum++;
+    }
+  }
+
+  const list = [];
+
+  for (const [key, val] of count) {
+    list.push(val);
+  }
+
+  list.sort((a, b) => b - a);
+
+  for (const num of list) {
+    if (k <= 0) return answer;
+    k -= num;
+    answer++;
+  }
+  return answer;
+}
