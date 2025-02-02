9e81d400963fdd2ec6b6e96194652664d92f3471
candymask0712 <candymask0712@gmail.com>
2025-02-03 08:36:19 +0900

    solve: 5챕터 문제 풀이

--- 연습장.js
+++ 연습장.js
@@ -1,19 +1,38 @@
-// * 5-4
-function solution(k, arr) {
-  let rp = 0;
-  const n = arr.length;
+const solution = (s, t) => {
   let answer = 0;
-  for (let lp = 0; lp < n; lp++) {
-    let sum = arr[lp];
-    let rp = lp;
-    while (sum <= k && rp < n) {
-      answer++;
-      rp++;
-      sum += arr[rp];
-    }
+  let window = s.slice(0, 3);
+
+  const n = s.length;
+
+  for (let i = 3; i <= n; i++) {
+    console.info(i, s[i], window, t, isAna(window, t));
+    if (isAna(window, t)) answer++;
+    window = window.slice(1) + s[i];
   }
   return answer;
+};
+
+function isAna(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  const h = new Map();
+  for (const char of str1) {
+    if (h.has(char)) h.set(char, h.get(char) + 1);
+    else h.set(char, 1);
+  }
+
+  for (const char of str2) {
+    if (!h.has(char)) return false;
+    else h.set(char, h.get(char) - 1);
+  }
+
+  for (const [key, val] of h) {
+    if (val !== 0) return false;
+  }
+
+  return true;
 }
 
-let arr = [1, 3, 1, 2, 3];
-console.log(solution(5, arr)); // 10
+const str6 = 'bacaAacba';
+const target = 'abc';
+
+console.info(solution(str6, target));
