fc4c4a94ed2355e074819184ef003b5360eb8120
candymask0712 <candymask0712@gmail.com>
2025-08-24 18:22:39 +0900

    feat: 문제풀이

---
+++ Stack&Queue/Queue/Leet-easy-number-of-recent-calls.js
@@ -0,0 +1,38 @@
+// * 문제
+// *
+
+//
+//
+
+// * 모범 답안
+
+// * 3차 풀이
+
+// * 2차 풀이
+
+// * 1차 풀이 - 정답 보고 풀이
+
+var RecentCounter = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  this.queue.push(t);
+  // console.log(t ,this.queue)
+  while (this.queue[0] < t - 3000) {
+    this.queue.shift();
+  }
+  console.log(this.queue.length);
+
+  return this.queue.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
