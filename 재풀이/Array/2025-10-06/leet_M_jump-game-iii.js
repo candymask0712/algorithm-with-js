efe00ef8b261a732ac7324f4a6cae158b3954685
roger-service-fe <roger.kim@zent.kr>
2025-12-04 21:54:57 +0900

    feat: 문제풀이

--- 재풀이/Array/2025-10-06/leet_M_jump-game-iii.js
+++ 재풀이/Array/2025-10-06/leet_M_jump-game-iii.js
@@ -8,7 +8,30 @@
 
 // * 3차 풀이
 
-// * 2차 풀이 - (25.10.03) - 정답
+// * 2차 풀이 - (25.12.04) - 성공
+var canReach = function (arr, start) {
+  const n = arr.length;
+  const isVisited = Array(n).fill(false);
+
+  const q = [];
+
+  q.push(start);
+
+  while (q.length) {
+    const cur = q.shift();
+    if (arr[cur] === 0) return true;
+    const range = arr[cur];
+    if (cur + range < n && !isVisited[cur + range]) {
+      isVisited[cur + range] = true;
+      q.push(cur + range);
+    }
+    if (cur - range >= 0 && !isVisited[cur - range]) {
+      isVisited[cur - range] = true;
+      q.push(cur - range);
+    }
+  }
+  return false;
+};
 
 // * 1차 풀이 - (25.10.03) - 성공
 // ?	시간복잡도: O(2^n)
