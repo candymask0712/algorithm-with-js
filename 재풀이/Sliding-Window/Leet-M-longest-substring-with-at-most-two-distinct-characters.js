dbdda146dc3850d433ccf58f8481ce49ac9ccace
roger-service-fe <roger.kim@zent.kr>
2026-01-01 23:31:31 +0900

    feat: 문제풀이

---
+++ 재풀이/Sliding-Window/Leet-M-longest-substring-with-at-most-two-distinct-characters.js
@@ -0,0 +1,43 @@
+// * 문제 링크
+
+// * 3차 풀이
+//? 시간복잡도: O()
+//? 공간복잡도: O()
+
+// * 2차 풀이
+//? 시간복잡도: O()
+//? 공간복잡도: O()
+
+// * 1차 풀이
+//? 시간복잡도: O()
+//? 공간복잡도: O()
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstringTwoDistinct = function (s) {
+  let lp = 0;
+  const h = new Map();
+  let answer = 0;
+
+  for (let rp = 0; rp < s.length; rp++) {
+    const cur = s[rp];
+    h.set(cur, (h.get(cur) || 0) + 1);
+    while (h.size > 2) {
+      const prev = s[lp];
+      if (h.get(prev) === 1) {
+        h.delete(prev);
+      } else {
+        h.set(prev, h.get(prev) - 1);
+      }
+      lp++;
+    }
+    answer = Math.max(answer, rp - lp + 1);
+  }
+
+  return answer;
+};
+
+// * 참고 풀이
+//? 시간복잡도: O()
+//? 공간복잡도: O()
